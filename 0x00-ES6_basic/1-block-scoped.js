3713abde447a59d3c474fc6456b46427c1a46f67
FaithO3321 <faithomondi1331@gmail.com>
2024-06-24 23:14:35 +0000

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,16 @@
 export default function taskBlock(trueOrFalse) {
+  // Declare constants outside the if block
+  // These will be the values returned by the function
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
+    // These constants are block-scoped and don't affect the outer constants
+    // They are separate variables with the same names
     const task = true;
     const task2 = false;
   }
 
+  // Always returns the outer constants, regardless of the if block execution
   return [task, task2];
 }
