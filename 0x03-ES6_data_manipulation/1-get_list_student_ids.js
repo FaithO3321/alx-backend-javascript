92e2460fbec6a69673c0917664a86d7abeb2ed09
FaithO3321 <faithomondi1331@gmail.com>
2024-07-19 22:27:30 +0000

    all tasks

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+/*eslint-disable*/
+export default function getListStudentIds(listStudents) {
+  if (!Array.isArray(listStudents)) {
+    return [];
+  }
+
+  return listStudents.map(student => student.id);
+}
